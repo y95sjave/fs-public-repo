d242a8eb49d490ea54ea5fa176e11fd0f88b375b
Jani Veikkolainen <jani.veikkolainen@gmail.com>
2024-12-09 17:04:20 +0200

    3.21

--- index.js
+++ index.js
@@ -53,14 +53,33 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
+  //const person = persons.find(person => person.id === id)
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  console.log("index.js app.put")
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number
   }
+
+  Person.findByIdAndUpdate(request.params.id, person)
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -68,7 +87,7 @@ const generateId = () => {
   return String(randomId)
 }
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   console.log("index.js app.post")
   const body = request.body
 
@@ -78,14 +97,14 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  if (persons.find(person => person.name === body.name)) {
+  /*if (persons.find(person => person.name === body.name)) {
     return response.status(400).json({
       error: 'name already exists, must be unique'
     })
-  }
+  }*/
 
   const person = new Person({
-    id: generateId(),
+  //  id: generateId(),
     name: body.name,
     number: body.number
   })
@@ -97,15 +116,35 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
   //morgan.token('body', request => JSON.stringify(request.body))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
+app.delete('/api/persons/:id', (request, response, next) => {
+  /*const id = request.params.id
   persons = persons.filter(person => person.id !== id)
+  response.status(204).end()*/
 
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error("ERROR:",error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
--- models/person.js
+++ models/person.js
@@ -13,8 +13,19 @@ mongoose.connect(url)
   })
 
   const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+      type: String,
+      minlength: 3
+    },
+    number: {
+      type: String,
+      validate: {
+        validator: function(v) {
+          return /^\d{2,3}-\d+$/.test(v)
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      }
+    }
   })
   
   personSchema.set('toJSON', {
